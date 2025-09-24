0b2efa3e1fca71125093ab97daedf34182246176
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2025-09-24 09:38:24 +0100

    Update timetable.js

--- timetable.js
+++ timetable.js
@@ -3,4 +3,5 @@ document.getElementById("test").onclick = testFunc;
 function testFunc() {
   var text = prompt("Whats your name? ")
   alert(text)
+  document.cookie = `username=${text}; expires=Wed, 1 Jul 2026 12:00:00 UTC`;
 }
