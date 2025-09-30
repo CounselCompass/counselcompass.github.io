71bff7c85067d9615358ddc5eccf0edd5eed109a
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2025-09-30 12:58:46 +0100

    test cookies

--- timetable.js
+++ timetable.js
@@ -1,7 +1,6 @@
 document.getElementById("test").onclick = testFunc;
 
 function testFunc() {
-  var text = prompt("Whats your name? ")
-  alert(text)
-  document.cookie = `username=${text}; expires=Wed, 1 Jul 2026 12:00:00 UTC`;
+  alert(document.cookie)
+  // document.cookie = `username=${text}; expires=Wed, 1 Jul 2026 12:00:00 UTC`;
 }
