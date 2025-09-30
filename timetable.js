cfd265f59c4356c27efa77b23fbec5cb38ca118f
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2025-09-30 13:04:59 +0100

    Only display value

--- timetable.js
+++ timetable.js
@@ -1,6 +1,6 @@
 document.getElementById("test").onclick = testFunc;
 
 function testFunc() {
-  alert(document.cookie)
+  alert(document.cookie.slice(9)
   // document.cookie = `username=${text}; expires=Wed, 1 Jul 2026 12:00:00 UTC`;
 }
